faab2b742c1ce2bd704b7379a4086a51aa088056
lcm <lemmiix@gmail.com>
2025-10-09 23:38:40 +0200

    make even more relevant info common

--- src/common.h
+++ src/common.h
@@ -2,10 +2,12 @@
 #define COMMON_H
 
 #include "SDL3/SDL_render.h"
+#include "c_map.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define FONT "terminal-f4.ttf"
+#define MOVEMENT_SPEED 200.0f
 
 static const char* signature = R"(
                                                   
@@ -32,9 +34,10 @@ static const char* signature = R"(
 extern float delta_time;
 extern SDL_Renderer* renderer;
 extern SDL_Window* window;
+extern SDL_Vertex reference_triangle[];
 
-
-// library definition
-#include "c_map.h"
+SDL_Point get_player_pos();
+SDL_Point get_mouse_pos();
+float get_player_rot();
 
 #endif
